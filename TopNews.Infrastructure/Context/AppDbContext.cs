f3563120cd3c03a9652a28febbbce2b8ea2a3ed3
dev <dev@A33PC3>
2023-07-07 18:12:45 +0300

    Add project files.

---
+++ TopNews.Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopNews.Infrastructure.Context
+{
+    internal class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext() : base()  { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)  { }
+
+    }
+}
---
+++ TopNews.Infrastructure/ServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopNews.Infrastructure.Context;
+
+namespace TopNews.Infrastructure
+{
+    public static class ServiceExtensions
+    {
+        public static void AddDbContext(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseSqlServer(connectionString);
+                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+        }
+    }
+}
